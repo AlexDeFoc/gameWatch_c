48d95696928cc7766dfddf0a601974637ca92350
AlexDeFoc <alex.de.foc@gmail.com>
2024-10-19 15:39:39 +0300

    1.0.2 optimiz

--- src/main.c
+++ src/main.c
@@ -6,60 +6,18 @@
 #define AUTO_SAVE_MINUTES 5
 #define MAX_TITLE_LENGHT 50
 
-void print_menu_options() {
-    char *menu_items[MENU_OPTIONS] = {
-        "List",
-        "Start",
-        "New",
-        "Remove"
-    };
-
-    for (int i = 0; i < MENU_OPTIONS; i++) {
-        printf("%d. %s\n", i+1, menu_items[i]);
-    }
-
-    printf("-> ");
-}
-
-void get_user_menu_input(int *container) {
-    int ch;
-    while (1) {
-        ch = getc(stdin);
-
-        while (getc(stdin) != '\n'); // flush
-
-        if (ch >= '1' && ch <= MENU_OPTIONS + '0') {
-            *container = ch - '0'; // convert ascii to int
-            break;
-        } else {
-            printf("Invalid input!\n");
-        }
-    }
-}
-
-void get_title_name(char **name_buf, int *lenght) {
-    printf("Enter name: ");
-
-    int ch = 0;
-    int i = 0;
-    while (1) {
-        ch = getc(stdin);
-        if (ch < 0 || ch == '\n') break;
-
-        (*name_buf)[i] = (char)ch;
-        i++;
-    }
-
-    (*name_buf)[i] = '\0';
-    *lenght = i;
-}
+void clear_screen();
+void hold_screen();
+void print_menu_options();
+void get_user_menu_input(int *container);
+void get_title_name(char *name_buf);
 
 int main() {
     char title_buffer[MAX_TITLE_LENGHT] = {0};
     int time_buffer = 0;
 
     enum menu_opts {
-        List = 1, Start, Stop, New, Remove
+        List = 1, Start, New, Remove
     };
 
     const char *record_path = "./record.txt";
@@ -73,7 +31,6 @@ int main() {
 
     int menu_ui = 0;
     char *name_buffer = malloc(MAX_TITLE_LENGHT * sizeof(char));
-    int name_lenght = 0;
     if (name_buffer == NULL) {
         printf("No memory!\n");
         return 1;
@@ -109,7 +66,7 @@ int main() {
         fseek(record, 0, SEEK_SET);
         fclose(record);
     } else if (menu_ui == New) {
-        get_title_name(&name_buffer, &name_lenght);
+        get_title_name(name_buffer);
         fopen_s(&record, record_path, "a");
 
         fprintf_s(record, ITEM_FORMAT_EXPORT, name_buffer, 0);
@@ -247,5 +204,62 @@ int main() {
         fclose(record);
     }
 
+    hold_screen();
+
     return 0;
+}
+
+void clear_screen() {
+    printf("\e[1;1H\e[2J");
+}
+
+void hold_screen() {
+    getc(stdin);
+}
+
+void print_menu_options() {
+    char *menu_items[MENU_OPTIONS] = {
+        "List",
+        "Start",
+        "New",
+        "Remove"
+    };
+
+    for (int i = 0; i < MENU_OPTIONS; i++) {
+        printf("%d. %s\n", i+1, menu_items[i]);
+    }
+
+    printf("-> ");
+}
+
+void get_user_menu_input(int *container) {
+    int ch;
+    while (1) {
+        ch = getc(stdin);
+
+        while (getc(stdin) != '\n'); // flush
+
+        if (ch >= '1' && ch <= MENU_OPTIONS + '0') {
+            *container = ch - '0'; // convert ascii to int
+            break;
+        } else {
+            printf("Invalid input!\n");
+        }
+    }
+}
+
+void get_title_name(char *name_buf) {
+    printf("Enter name: ");
+
+    int ch = 0;
+    int i = 0;
+    while (1) {
+        ch = getc(stdin);
+        if (ch == '\n' || ch < 0) break;
+
+        name_buf[i] = (char)ch;
+        i++;
+    }
+
+    name_buf[i] = '\0';
 }
